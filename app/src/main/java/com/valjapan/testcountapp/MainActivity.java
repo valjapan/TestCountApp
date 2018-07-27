1f7618e7651bcd117db4bb2f13bfed7232349016
yukiyuki961 <nabe@M-Yuki-Nabeshima-MacBookPro.local>
2018-07-28 00:50:02 +0900

    first commit

---
+++ app/src/main/java/com/valjapan/testcountapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.valjapan.testcountapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+    }
+
+    public void plus(View v){
+        i = i + 1;
+        textView.setText(String.valueOf(i));
+    }
+}
